94b611116ae0911518720fcccb0e2d990b8c7dec
jloves9191 <jkwon.uxdeveloper@gmail.com>
2021-03-10 00:53:14 +0900

    git test 3

--- src/pages/Welcome/pc/PcWelcome.jsx
+++ src/pages/Welcome/pc/PcWelcome.jsx
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
             fontFamily: 'NanumPen',
-            fontSize: '150%',asdf
+            fontSize: '150%',
         },
     },
 }));
